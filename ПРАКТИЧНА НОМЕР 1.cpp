21b30b37dc52df6d9b926ba3b7bd0c936bf0aff1
vaskivskyi-v <vaskivskyi-v@class.ztk.org.ua>
2025-11-07 20:15:26 +0200

    Add files via upload

---
+++ ПРАКТИЧНА НОМЕР 1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int count = 0;   // замніа каунт
+	count += 1;      // плюс 1
+	count += 3;      // каунт плюс 3
+	cout << count << endl;  // результат
+	system("pause");  // щоб консоль не закрилась одразу після виконання
+	return 0;
+}
